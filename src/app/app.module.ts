6b0cb9069bf9f3840c2be107ef3b7d521575462b
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-02-07 15:09:32 +0100

    Commit inicial con error al intentar importar los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { StatisticsComponent } from './ingreso-egreso/statistics/statistics.component';
+import { DetailsComponent } from './ingreso-egreso/details/details.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    StatisticsComponent,
+    DetailsComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ingreso-egreso/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: false,
+  templateUrl: './details.component.html',
+  styles: ``
+})
+export class DetailsComponent {
+
+}
